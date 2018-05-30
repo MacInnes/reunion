861e20fec8c3d019ab2044428496e92749d20084
Andrew Mockett <califormula@gmail.com>
2018-05-30 10:37:00 -0600

    iteration 4

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -2,13 +2,41 @@ class Reunion
   attr_reader :location,
               :activities
 
-  def initialize(location, activities)
+  def initialize(location, activities = [])
     @location = location
-    @activities = activities || []
+    @activities = activities
   end
 
   def add_activity(activity)
     @activities << activity
+  end
+
+  def total_cost
+    @activities.reduce(0) do |total, activity|
+      total += activity.total_cost
+      total
+    end
+  end
+
+  def total_amount_owed_per_participant
+    owed_array = @activities.map do |activity|
+      activity.amount_owed
+    end
+    owed_array.reduce({}) do |collector, current|
+      current.each do |key, value|
+        if collector[key]
+          collector[key] += value
+        else
+          collector[key] = value
+        end
+      end
+      collector
+    end
+  end
+
+  def print_summary
+    puts total_amount_owed_per_participant
+    total_amount_owed_per_participant # added this just to test for output...  tho it was already tested above...?  idk
   end
 
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -29,4 +29,59 @@ class ReunionTest < Minitest::Test
     assert_equal 3, reunion.activities.length
   end
 
+  def test_total_reunion_cost
+    golf = Activity.new("Golf", 200, 50)
+    swimming = Activity.new("Swimming", 5, 5)
+
+    reunion = Reunion.new("Boulder", [golf, swimming])
+
+    golf.add_participant("Andrew", 50)
+    golf.add_participant("JR", 75)
+    golf.add_participant("rich guy", 275)
+
+    swimming.add_participant("Andrew", 5)
+    swimming.add_participant("JR", 10)
+    swimming.add_participant("rich guy", 10)
+
+    actual = reunion.total_cost
+    assert_equal 370, actual
+  end
+
+  def test_total_amount_owed_per_participant
+    camping = Activity.new("Camping", 10, 10)
+    bbq = Activity.new("BBQ", 20, 20)
+
+    camping.add_participant("Andrew", 30)
+    camping.add_participant("JR", 10)
+    bbq.add_participant("Andrew", 30)
+    bbq.add_participant("JR", 40)
+
+    reunion = Reunion.new("Conifer", [camping, bbq])
+
+    expected = {
+      "Andrew" => -15,
+      "JR" => -5
+    }
+
+    assert_equal expected, reunion.total_amount_owed_per_participant
+  end
+
+  def test_print_summary # this test is bad, and I should feel bad
+    camping = Activity.new("Camping", 10, 10)
+    bbq = Activity.new("BBQ", 20, 20)
+
+    camping.add_participant("Andrew", 30)
+    camping.add_participant("JR", 10)
+    bbq.add_participant("Andrew", 30)
+    bbq.add_participant("JR", 40)
+
+    reunion = Reunion.new("Conifer", [camping, bbq])
+
+    expected = {
+      "Andrew" => -15,
+      "JR" => -5
+    }
+    assert_equal expected, reunion.print_summary
+  end
+
 end
