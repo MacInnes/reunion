8e9a77d102c4faa52037fd658744167dd4b59b50
Andrew Mockett <califormula@gmail.com>
2018-05-30 09:57:56 -0600

    iteration 3

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -1,2 +1,14 @@
 class Reunion
+  attr_reader :location,
+              :activities
+
+  def initialize(location, activities)
+    @location = location
+    @activities = activities || []
+  end
+
+  def add_activity(activity)
+    @activities << activity
+  end
+
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -2,6 +2,31 @@ require 'minitest'
 require 'minitest/test'
 require 'minitest/autorun'
 require './lib/reunion'
+require './lib/activity'
 
 class ReunionTest < Minitest::Test
+
+  def test_has_attributes
+    golf = Activity.new("Golf", 200, 50)
+    swimming = Activity.new("Swimming", 5, 5)
+
+    reunion = Reunion.new("Boulder", [golf, swimming])
+
+    assert_equal "Boulder", reunion.location
+    assert_equal 2, reunion.activities.length
+  end
+
+  def test_can_add_activities
+    golf = Activity.new("Golf", 200, 50)
+    swimming = Activity.new("Swimming", 5, 5)
+
+    reunion = Reunion.new("Boulder", [golf, swimming])
+
+    hiking = Activity.new("Hiking", 25, 20)
+
+    reunion.add_activity(hiking)
+
+    assert_equal 3, reunion.activities.length
+  end
+
 end
